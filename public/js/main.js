37cbb84a0d5a9b725b7205d552acba9fe426aaae
scheuerkev <scheuer.kevin@gmail.com>
2022-01-16 15:48:27 +0100

    Transpiling JS and postfixed CSS

--- public/js/main.js
+++ public/js/main.js
@@ -1,15 +1,20 @@
-//Targeting all faq items in an array
+"use strict";
 
-const question = document.querySelectorAll(".faq__content");
+//Targeting all faq items in an array
+var question = document.querySelectorAll(".faq__content");
 
 //Adding a click listener on each one of the array, getting the childs (a node list)
 //of each element and toggling each class on proper elements
 
-for (let i = 0; i < question.length; i++) {
-  question[i].addEventListener("click", () => {
-    let childs = question[i].childNodes;
+var _loop = function _loop(i) {
+  question[i].addEventListener("click", function () {
+    var childs = question[i].childNodes;
     question[i].classList.toggle("active__content");
     childs[1].classList.toggle("active__question");
     childs[3].classList.toggle("active__answer");
   });
+};
+
+for (var i = 0; i < question.length; i++) {
+  _loop(i);
 }
